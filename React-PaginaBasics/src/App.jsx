17d2b069a555588c5612288fb9c322c56eaf0d9d
JexonMG <jexon.mejias@unadeca.net>
2024-05-19 12:22:52 -0600

    commit

--- React-PaginaBasics/src/App.jsx
+++ React-PaginaBasics/src/App.jsx
@@ -17,28 +17,57 @@ import FormLabel from '@mui/material/FormLabel'
 import Table from '@mui/material/Table'
 import BasicTable from './assets/Table'
 import BarraDeApp from './assets/BarraDeApp'
+import React, { useState, useEffect } from 'react';
+
 //Shawncito, 
 
 function App() {
+  const [nombre, setnombre] = useState('')
+  const [sobrenombre, setsobrenombre] = useState('')
+  const [foto, setfoto] = useState('')
+  const [checked, setChecked] = useState(false);
+  const [datos, setDatos] = useState([])
+
+  const handleChange = (setTextValue) => (event) => {
+    setTextValue(event.target.value);
+  };
+
+  const handleChangeSwitch = (event) => {
+    setChecked(event.target.checked);
+  };
+
+  const guardar = () => {
+    nombre === '' || sobrenombre === '' || foto === '' ? alert('Faltan datos') :
+    setDatos([...datos, {nombre, sobrenombre, foto, mejorAmigo: checked}])
+  }
+
+  console.log( nombre)
+  console.log(sobrenombre)
+  console.log(foto)
+  console.log(checked)
+
   return (
     <div>
       <Grid>
         <BarraDeApp/>
-        <div className='Main'>
+        <div className='Main' style={{ padding: '20px' }}>
         <Card>
           <Typography>
             <h3>Agregar nuevo</h3>
-          </Typography>
-            <TextField label='Nombre' variant='outlined'/>
-            <TextField label='Sobrenombre' variant='outlined'/>
-            <TextField label='Foto' variant='outlined'/>
-          <Stack direction='row'>
-            <Switch/>
-            <FormLabel>Es mejor amigo</FormLabel>
+            </Typography>
+          <div className='Main'>
+              <TextField label='Nombre' variant='outlined' value={nombre} onChange={handleChange(setnombre)}/>
+              <TextField label='Sobrenombre' variant='outlined' value={sobrenombre} onChange={handleChange(setsobrenombre)}/>
+              <TextField label='Foto' variant='outlined' value={foto} onChange={handleChange(setfoto)}/>
+
+          </div>  
+          <Stack direction="row" spacing={1} alignItems="center">
+            <Switch onChange={handleChangeSwitch}/>
+            <FormLabel>¿Es mejor amigo?</FormLabel>
           </Stack>
-          <Stack>
-            <Button size='small' variant='contained'>GUARDAR</Button>
-            <BasicTable/>
+          <Stack spacing={2}>
+            <Button size='small' variant='contained' onClick={guardar}>GUARDAR</Button>
+            <BasicTable value= {datos}/>
             </Stack>
         </Card>
         </div>
--- React-PaginaBasics/src/assets/Table.jsx
+++ React-PaginaBasics/src/assets/Table.jsx
@@ -6,13 +6,11 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { Typography } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
 
-export default function BasicTable() {
-    function createData(foto, nombre, sobrenombre, mejoramigo) {
-        return { foto, nombre, sobrenombre, mejoramigo};
-    }
-    
-    
+export default function BasicTable(datos) {
+
     return (
         <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table" size='small'>
@@ -20,10 +18,35 @@ export default function BasicTable() {
             <TableRow>
                 <TableCell>Nombre</TableCell>
                 <TableCell align="right">Sobrenombre</TableCell>
-                <TableCell align="right">Foto</TableCell>
+                <TableCell align="center">Foto</TableCell>
                 <TableCell align="right">Mejor amigo</TableCell>
             </TableRow>
             </TableHead>
+            <TableBody>
+            {datos.value.length === 0 ? (<TableRow>
+                <TableCell colSpan={4} align="center">
+                    <Typography>
+                        No hay datos
+                    </Typography>
+                </TableCell>
+                </TableRow>):
+            (datos.value.map((row) => (
+
+                <TableRow
+                key={row.nombre}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                <TableCell component="th" scope="row">
+                  {row.nombre}
+                </TableCell>
+                <TableCell align="right">{row.sobrenombre}</TableCell>
+                <TableCell align="center">
+                  <Avatar alt={row.sobrenombre} src={row.foto}  sx={{ display: 'flex', justifyContent: 'center' }}/>
+                </TableCell>
+                <TableCell align="right">{row.mejorAmigo ? 'Si' : 'No'}</TableCell>
+                </TableRow>
+            )))}
+            </TableBody>
         </Table>
         </TableContainer>
     );
