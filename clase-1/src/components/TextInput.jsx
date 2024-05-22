96e5a0085a55c3562d7202102515163263395545
JexonMG <jexon.mejias@unadeca.net>
2024-05-22 15:18:17 -0600

    Commit

--- clase-1/src/components/TextInput.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import TextField from '@mui/material/TextField';
-
-const TextInput = (props) => {
-  return(
-    <TextField type='text' onChange={props.handleName} variant='outlined' />
-  );
-}
-export default TextInput;
