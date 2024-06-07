0a4cda939d666d0f739e88a6d89a986e43ae4d41
Valeria Vega Herrera <carmen.vega@unadeca.net>
2024-06-06 19:58:36 -0600

    commit

--- CompasApp/src/pages/Amigo_Page/Amigo_Page.jsx
+++ CompasApp/src/pages/Amigo_Page/Amigo_Page.jsx
@@ -1,17 +1,37 @@
-import React from 'react'
-import "./Amigo_Page.css";
+import React from 'react';
+import { Box, Card, CardContent, Avatar, Typography, Button, Link } from '@mui/material';
+import {Link as RouterLink} from 'react-router-dom'
+import './Amigo_Page.css';
 
-export default function Amigo_Page({amigo}) {
+export default function Amigo_Page({ amigo }) {
+    console.log('')
+
     return (
-        <div className="amigo-page">
-            <h1>
-                {amigo.name}	
-            </h1>
-            <img src={`https://unavatar.io/${amigo.picture}`} alt={amigo.name} />
-            <p>
-                {amigo.nickname}
-            </p>
-            <a href="/" className="home-link">Ir a la página principal</a>
-        </div>
-    )
+        <Box className="amigo-page-container">
+            <Card className="amigo-page-card">
+                <CardContent>
+                    <Typography variant="h4" component="h1" gutterBottom color="text.primary">
+                        {amigo.name}
+                    </Typography>
+                    <Avatar
+                        alt={amigo.name}
+                        src={`https://unavatar.io/${amigo.picture}`}
+                        className="amigo-page-avatar"
+                    />
+                    <Typography variant="subtitle1" color="text.secondary" gutterBottom>
+                        {amigo.nickname}
+                    </Typography>
+                    <Button
+                        variant="contained"
+                        disableElevation
+                        className="amigo-page-button"
+                        component = {RouterLink}
+                        to = "/"
+                    >
+                        Ir a la página principal
+                    </Button>
+                </CardContent>
+            </Card>
+        </Box>
+    );
 }
